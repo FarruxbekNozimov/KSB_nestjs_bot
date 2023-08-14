ff8be323d5fdfacd13e7a4bcec377f30c22a64e1
FarruxDEV <farruxbeknozimov@gmail.com>
2023-08-14 21:19:30 +0500

    add error handler

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 3001;
+  try {
+    const app = await NestFactory.create(AppModule);
+    const PORT = process.env.PORT || 3001;
+    app.enableCors();
+    app.useGlobalPipes(new ValidationPipe());
 
-  app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: ['*'],
-    credentials: true,
-  });
+    app.use((req, res, next) => {
+      const startTime = Date.now();
+      res.on('finish', () => {
+        const endTime = Date.now();
+        const responseTime = endTime - startTime;
+        console.log(
+          `${req.method} ${req.originalUrl} ${res.statusCode} ${responseTime}ms`,
+        );
+      });
+      next();
+    });
 
-  await app.listen(PORT, () => {
-    console.log('server ishga tushdi');
-  });
+    await app.listen(PORT, () => {
+      console.log('server ishga tushdi');
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 bootstrap();
